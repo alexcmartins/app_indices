e3c4361b8781c28de21a2a54a3b0d639185d006d
Alex C. Martins <cantareiramartins@gmail.com>
2021-01-22 20:25:17 +0000

    inclusão readme e novos ajustes

--- modules/api_front.js
+++ modules/api_front.js
@@ -1,7 +1,13 @@
+/* Função que acessa o valor do input e incrementa ao link para fazer a requisição.
+É feita tratativa da resposta, após a mesma foi mapeada e com o retorno foi criada uma 
+nova div com os campos selecionados*/
+
 function valueList () {
     const inputElement = document.getElementById('choose').value
 
-    //fetch('http://localhost:3030/'+`${inputElement}`)
+    /*fetch('http://localhost:3030/'+`${inputElement}`) - 
+    Link utilizado internamente para desenvolvimento.*/
+
     fetch('http://cmdev.ddns.net:3030/'+`${inputElement}`)   
         .then(response => {
             if (!response.ok) {
@@ -32,18 +38,20 @@ function valueList () {
         })
         .catch(error => {
             console.log(error);
-        });
-
-    
+        });    
 };
 
-/*var wait = document.getElementById("quote");   
+/* Essa função seria para esperar o evento do click no botão cotar para chamar as funções abaixo.
+No momento essa função está desativada.
+
+var wait = document.getElementById("quote");   
 wait.addEventListener("click", valueList, false);*/
 
+// Função que acessa o Html5 no campo value do input e zera todos os valores.
 function valueReset() {
     const inputReset = document.getElementById('choose').value = "";
-    const clean = document.getElementById("price");
   
-    // Remove todos os descendentes da <div id="alvo">
+    // Remove todos os elemetos da <div id="price">
+    const clean = document.getElementById("price");
     clean.innerText = "";
 };
