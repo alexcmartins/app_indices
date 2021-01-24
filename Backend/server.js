171481711ef81f8d058b77cab19c2400155523cb
Alex C. Martins <cantareiramartins@gmail.com>
2021-01-24 21:50:18 +0000

    Alt. rotas estáticas para dinâmicas, alt. readme

--- Backend/server.js
+++ Backend/server.js
@@ -6,66 +6,12 @@ const port = 3030
 
 app.use(cors())
 
-app.get('/usd', async(req, res) => {
+/*Rota dinâmica*/
+app.get('/:currency', async(req, res) => {
 
     try {
-        const { data } = await axios('https://economia.awesomeapi.com.br/json/USD-BRL')
-        //(`https://economia.awesomeapi.com.br/json/${choose}`)
-        //('https://economia.awesomeapi.com.br/json/')
-
-        return res.json(data)
-    } catch (error) {
-        console.error(error)
-    }
-    
-});
-
-app.get('/eur', async(req, res) => {
-
-    try {
-        const { data } = await axios('https://economia.awesomeapi.com.br/json/EUR-BRL')
-        //(`https://economia.awesomeapi.com.br/json/${choose}`)
-        //('https://economia.awesomeapi.com.br/json/')
-
-        return res.json(data)
-    } catch (error) {
-        console.error(error)
-    }
-
-});
-
-app.get('/gbp', async(req, res) => {
-
-    try {
-        const { data } = await axios('https://economia.awesomeapi.com.br/json/GBP-BRL')
-        //(`https://economia.awesomeapi.com.br/json/${choose}`)
-        //('https://economia.awesomeapi.com.br/json/')
-
-        return res.json(data)
-    } catch (error) {
-        console.error(error)
-    }
-    
-});
-
-app.get('/btc', async(req, res) => {
-
-    try {
-        const { data } = await axios('https://economia.awesomeapi.com.br/json/BTC-BRL')
-        //(`https://economia.awesomeapi.com.br/json/${choose}`)
-        //('https://economia.awesomeapi.com.br/json/')
-
-        return res.json(data)
-    } catch (error) {
-        console.error(error)
-    }
-    
-});
-
-app.get('/eth', async(req, res) => {
-
-    try {
-        const { data } = await axios('https://economia.awesomeapi.com.br/json/ETH-BRL')
+        var currency = req.params.currency
+        const { data } = await axios(`https://economia.awesomeapi.com.br/json/${currency}`)
         //(`https://economia.awesomeapi.com.br/json/${choose}`)
         //('https://economia.awesomeapi.com.br/json/')
 
