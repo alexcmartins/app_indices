5ebb27fb836459b8656cefdff10eccca89d6facc
Alex Cantareira Martins <cantareiramartins@gmail.com>
2021-02-28 22:29:09 +0000

    Alt router

--- src/Routes/router.js
+++ src/Routes/router.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Sequelize = require('sequelize')
 const Coin = require('../Models/Coin');
 const router = express.Router();
 
@@ -12,13 +13,36 @@ router.get('/', async(req, res) => {
     
 });
 
-router.get('/v1/api', async(req, res) => {
+router.get('/v1/api/exchange', async(req, res) => {
     try {
         //var currency = req.params.currency
         const data = await Coin.findAll({
-            attributes: ['Cripto']
+            where: {
+                key: 'Exchange'
+            },
+            order: [['createdAT', 'DESC']],
+            limit: 9
         });
-        res.status(200).send(data)
+        res.status(200).json(data)
+    } catch (error) {
+        console.error(error)
+    }
+    
+});
+
+router.get('/v1/api/cripto', async(req, res) => {
+    try {
+        //var currency = req.params.currency
+        const data = await Coin.findAll({
+            where: {
+                key: 'Cripto'
+            },
+            order: [
+                ['id', 'DESC']
+            ],
+            limit: 14
+        });
+        res.status(200).json(data)
     } catch (error) {
         console.error(error)
     }
